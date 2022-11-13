a9558ea4cfc5242b69bf771f057e397f7f2d8fda
TonyGamer <tomblackaz@gmail.com>
2022-11-13 09:34:13 -0700

    decoupled AbstractHearingEntity from a specific mob type for future expandability

--- src/main/java/com/klarkson/creaternd/content/entity/sculk/FlintskinMob.java
+++ src/main/java/com/klarkson/creaternd/content/entity/sculk/FlintskinMob.java
@@ -1,14 +1,14 @@
 package com.klarkson.creaternd.content.entity.sculk;
 
 import com.klarkson.creaternd.content.entity.GeckoEntityHandler;
+import com.mojang.serialization.Dynamic;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -16,6 +16,8 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.gameevent.DynamicGameEventListener;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -25,11 +27,14 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.network.ISyncable;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class FlintskinMob extends AbstractHearingEntity implements IAnimatable, ISyncable {
+import java.util.function.BiConsumer;
+
+public class FlintskinMob extends Animal implements IAnimatable, ISyncable {
     private AnimationFactory factory = GeckoLibUtil.createFactory(this);
+    private HearingHelper hearingHelper;
 
     public FlintskinMob(EntityType<? extends Animal> type, Level level) {
-        super(type, level, 40);
+        super(type, level);
     }
 
     @Nullable
@@ -53,14 +58,40 @@ public class FlintskinMob extends AbstractHearingEntity implements IAnimatable, 
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 25.0D).add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
+    // TODO: Make a custom sound effect
+    public void signalReceived(ServerLevel level, Entity causalEntity) {
+        this.playSound(SoundEvents.SCULK_CLICKING, 1f, 1.2f+this.getVoicePitch());
+    }
+
+    public void tick() {
+        this.hearingHelper.tick();
+        super.tick();
+    }
+
+    public void customServerAiStep() {
+        this.hearingHelper.customServerAiStep();
+        super.customServerAiStep();
+    }
+
     public static boolean canSpawn(EntityType<FlintskinMob> entityType, LevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
         return checkAnimalSpawnRules(entityType, level, spawnType, pos, random) && pos.getY() > 100;
     }
 
-    // TODO: Make a custom sound effect
-    @Override
-    public void signalReceived() {
-        this.playSound(SoundEvents.SCULK_CLICKING, 1f, 1.2f+this.getVoicePitch());
+    public void addAdditionalSaveData(CompoundTag tag) {
+        this.hearingHelper.addAdditionalSaveData(tag);
+    }
+
+    public void readAdditionalSaveData(CompoundTag tag) {
+        this.hearingHelper.readAdditionalSaveData(tag);
+    }
+
+    public @NotNull Brain<?> makeBrain(Dynamic<?> dynamicBrain) {
+        this.hearingHelper = new HearingHelper(this, this::signalReceived, 40);
+        return this.hearingHelper.makeBrain(dynamicBrain);
+    }
+
+    public void updateDynamicGameEventListener(@NotNull BiConsumer<DynamicGameEventListener<?>, ServerLevel> gameEventFunction) {
+        this.hearingHelper.updateDynamicGameEventListener(gameEventFunction);
     }
 
     @Override
